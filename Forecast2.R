62f1dd4b11feae58684d6519c40a8006b126f4a8
Benjamin Fisher <benjaminfisher@Benjamins-MacBook-Pro.local>
2017-02-28 12:28:06 -0700

    Updated forecast script

--- Forecast2.R
+++ Forecast2.R
@@ -8,10 +8,26 @@ callByDay <- read_feather("callByDay.feather")
 
 m <- prophet(callByDay)
 
-future <- make_future_dataframe(m, periods = 365)
+future <- make_future_dataframe(m, periods = 730)
 tail(future)
 
 forecast <- predict(m, future)
 tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
 
+# Add holidays (add more...)
+
+holidays <- data_frame(
+  holiday = 'holidays',
+  ds = as.Date(c('2015-01-01', '2016-01-01', '2015-12-31', '2016-12-31',
+                 '2015-12-25', '2016-12-25', '2015-07-04', '2016-07-04',
+                 '2015-05-05', '2016-05-05')),
+  lower_window = -1,
+  upper_window = 1
+)
+
+m <- prophet(df, holidays = holidays)
+forecast <- predict(m,future)
+
 plot(m, forecast)
+
+prophet_plot_components(m, forecast)
