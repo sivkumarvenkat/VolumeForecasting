ca056ef4a403e19d42c6283a6d4ae536cf1c0912
Benjamin Fisher <benjaminfisher@Benjamins-MacBook-Pro.local>
2017-02-27 19:42:15 -0700

    Changed package attach-detach order

--- Forecast1.R
+++ Forecast1.R
@@ -4,9 +4,8 @@ library(data.table)
 # Read data using Data.Table::fread, then detach data.table if there are package conflicts
 EMScalls <- fread('EMScalls.csv')
 
-detach("package:data.table", unload=TRUE)
-
 library(dplyr)
+detach("package:data.table", unload=TRUE)
 
 # The script below follows the Prophet quick-start guide
 
