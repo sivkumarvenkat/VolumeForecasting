e17c2c092e828256f47f8d585eaa2ef7ee0c27a8
Benjamin Fisher <benjaminfisher@Benjamins-MacBook-Pro.local>
2017-02-27 17:22:42 -0700

    Updated script

--- Forecast1.R
+++ Forecast1.R
@@ -1,12 +1,17 @@
 library(prophet)
 library(data.table)
 
-# Read data using Data.Table::fread, then detach data.table
+# Read data using Data.Table::fread, then detach data.table if there are package conflicts
 EMScalls <- fread('EMScalls.csv')
-#detach("package:data.table", unload=TRUE)
+
+detach("package:data.table", unload=TRUE)
+
+library(dplyr)
 
 # The script below follows the Prophet quick-start guide
 
+EMScalls <- mutate(EMScalls, y = log(y))
+
 m <- prophet(EMScalls)
 
-future <- make_future_dataframe(m, periods = 365)
+future <- make_future_dataframe(m, periods = 760)
